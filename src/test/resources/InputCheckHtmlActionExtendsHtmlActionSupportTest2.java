1bf2a2e3d6fc1fa10df7f1542e0d67d3fdd67017
Paulo Merson <Paulo Merson>
2017-04-30 22:29:52 -0300

    no extends

--- src/test/resources/InputCheckHtmlActionExtendsHtmlActionSupportTest2.java
+++ src/test/resources/InputCheckHtmlActionExtendsHtmlActionSupportTest2.java
@@ -1,6 +1,6 @@
 package com.sun.j2ee.blueprints.consumerwebsite.actions;
 
-public class DummyHTMLAction extends HTMLActionSupport {
+public class DummyHTMLAction {
 
     public void doNothing() {
     }
